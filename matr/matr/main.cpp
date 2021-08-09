de0b1465dbcab7891d5359ba0c16a9077704f0f5
murashko12 <pert@ocumare.ru>
2021-08-09 21:48:51 +0300

    matrix output function

--- matr/matr/main.cpp
+++ matr/matr/main.cpp
@@ -13,10 +13,20 @@ int **Matrix_in(const int size1, const int size2) // input matrix
     return matr;
 }
 
+void Matrix_out(const int size1, const int size2, int **matr) // output matrix
+{
+    for (int i = 0; i < size1; i++)
+    {
+        for (int j = 0; j < size2; j++)
+            printf("%4d", matr[i][j]);
+        printf("\n");
+    }
+}
 int main()
 {
     int N,M; // matrix size
     scanf("%d %d",&N, &M);
     int **Matrix = Matrix_in(N, M);
+    Matrix_out(N, M, Matrix);
     return 0;
 }

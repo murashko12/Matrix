25c74727f3051bad9188529be79fa5f8c5eda58a
murashko12 <pert@ocumare.ru>
2021-08-09 21:34:53 +0300

    matrix input function

--- matr/matr/main.cpp
+++ matr/matr/main.cpp
@@ -1,7 +1,22 @@
 #include <iostream>
 using namespace std;
 
+int **Matrix_in(const int size1, const int size2) // input matrix
+{
+    int **matr;
+    matr = new int *[size1];
+    for (int i = 0; i < size1; i++)
+        matr[i] = new int [size2];
+    for (int i = 0; i < size1; i++)
+        for (int j = 0; j < size2; j++)
+            scanf("%d", &matr[i][j]);
+    return matr;
+}
+
 int main()
 {
+    int N,M; // matrix size
+    scanf("%d %d",&N, &M);
+    int **Matrix = Matrix_in(N, M);
     return 0;
 }

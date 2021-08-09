0d88b3f77084a16707f5fdc4507144535eb8898b
murashko12 <pert@ocumare.ru>
2021-08-09 21:03:33 +0300

    the nain file is ready

---
+++ matr/matr/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  matr
+//
+//  Created by Петр Мурашко on 09.08.2021.
+//  Copyright © 2021 oooppp. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
